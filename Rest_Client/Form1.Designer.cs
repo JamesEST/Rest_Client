7defb4930fda863efdc2c6a92064a7c832661e72
Vladislav <Vladislav.narozni@gmail.com>
2021-11-12 12:19:25 +0200

    Create a app

---
+++ Rest_Client/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Rest_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtRestURI = new System.Windows.Forms.TextBox();
+            this.txtResponse = new System.Windows.Forms.TextBox();
+            this.cmdGO = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtRestURI
+            // 
+            this.txtRestURI.Location = new System.Drawing.Point(118, 6);
+            this.txtRestURI.Name = "txtRestURI";
+            this.txtRestURI.Size = new System.Drawing.Size(348, 22);
+            this.txtRestURI.TabIndex = 0;
+            // 
+            // txtResponse
+            // 
+            this.txtResponse.Location = new System.Drawing.Point(118, 35);
+            this.txtResponse.Multiline = true;
+            this.txtResponse.Name = "txtResponse";
+            this.txtResponse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResponse.Size = new System.Drawing.Size(482, 223);
+            this.txtResponse.TabIndex = 1;
+            // 
+            // cmdGO
+            // 
+            this.cmdGO.Location = new System.Drawing.Point(472, 5);
+            this.cmdGO.Name = "cmdGO";
+            this.cmdGO.Size = new System.Drawing.Size(128, 23);
+            this.cmdGO.TabIndex = 2;
+            this.cmdGO.Text = "GO";
+            this.cmdGO.UseVisualStyleBackColor = true;
+            this.cmdGO.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Requiest URI:";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Response:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 270);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmdGO);
+            this.Controls.Add(this.txtResponse);
+            this.Controls.Add(this.txtRestURI);
+            this.Name = "Form1";
+            this.Text = "C# REST Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+       
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Button cmdGO;
+
+        private System.Windows.Forms.Button cmd;
+
+        private System.Windows.Forms.TextBox txtResponse;
+        private System.Windows.Forms.TextBox txtRestURI;
+
+
+
+        #endregion
+    }
+}
